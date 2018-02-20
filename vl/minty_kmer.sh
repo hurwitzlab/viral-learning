cf9d37db39f4daf8c86ae0e43433710eec30a7a8
Joshua Lynch <jklynch@email.arizona.edu>
2018-02-20 15:57:47 +0000

    use more data

--- vl/minty_kmer.sh
+++ vl/minty_kmer.sh
@@ -7,4 +7,4 @@ python kmer_nn_np_generator.py \
     ${DATA_DIR}/bact_kmer_file2.fasta.tab.gz \
     ${DATA_DIR}/vir_kmer_file1.fasta.tab.gz \
     ${DATA_DIR}/vir_kmer_file2.fasta.tab.gz \
-    100
+    200
