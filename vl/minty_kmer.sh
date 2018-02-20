808047314831a15c0aecb35e0dfe2593d3fdab04
Joshua Lynch <jklynch@email.arizona.edu>
2018-02-20 11:19:04 +0000

    initial commit

---
+++ vl/minty_kmer.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+DATA_DIR=../data
+
+python kmer_nn_np_generator.py \
+    ${DATA_DIR}/bact_kmer_file1.fasta.tab.gz \
+    ${DATA_DIR}/bact_kmer_file2.fasta.tab.gz \
+    ${DATA_DIR}/vir_kmer_file1.fasta.tab.gz \
+    ${DATA_DIR}/vir_kmer_file2.fasta.tab.gz \
+    100
