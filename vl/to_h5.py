b503749b2bf54d330a98925616d4ed52515270f7
Joshua Lynch <jklynch@email.arizona.edu>
2018-02-26 21:12:58 -0500

    put more than one data set in the H5 file

--- vl/to_h5.py
+++ vl/to_h5.py
@@ -1,20 +1,58 @@
-import gzip
-import sys
+"""
+Read Alise's training and testing data files and smash them all into one H5 file.
+"""
+import os
 import time
 
 import h5py
 import numpy as np
 
 
-input_fp = sys.argv[1]  # '../data/bact_kmer_file1.fasta.tab.gz'
-h5_fp = sys.argv[2]  #'../data/bact_kmer_file1.h5'
-dset_name = sys.argv[3]
-row_count = int(sys.argv[4])
+data_source_dir = '/rsgrps/bhurwitz/alise/my_data/Machine_learning/size_read/contigs_training/set/cleanSet_Centrifuge'
 
 
-def read_tsv_write_h5(tsv_fp, h5_fp, dset_name, line_count):
+def write_all_training_and_testing_data():
+    h5_fp = '/extra/jklynch/viral-learning/training_testing.h5'
+    with h5py.File(h5_fp, 'w') as h5_file:
+        h5_file.create_group('/clean-bact/training1/extract/kmers')
+        h5_file.create_group('/clean-vir/training1/extract/kmers')
+        h5_file.create_group('/vir_marinePatric/extract_vir_100/kmers')
+        h5_file.create_group('/vir_marinePatric/extract_vir_200/kmers')
+        h5_file.create_group('/vir_marinePatric/extract_vir_500/kmers')
+        h5_file.create_group('/vir_marinePatric/extract_vir_1000/kmers')
+        h5_file.create_group('/vir_marinePatric/extract_vir_5000/kmers')
+        h5_file.create_group('/vir_marinePatric/extract_vir_10000/kmers')
+
+        training_line_count = 1000
+
+        read_tsv_write_h5_group(
+            tsv_fp=os.path.join(data_source_dir, 'clean-bact/training1/extract/kmers/kmer_file1.fasta.tab'),
+            h5_file=h5_file,
+            dset_name='clean-bact/training1/extract/kmers/kmer_file1',
+            line_count=training_line_count)
+
+        read_tsv_write_h5_group(
+            tsv_fp=os.path.join(data_source_dir, 'clean-bact/training1/extract/kmers/kmer_file2.fasta.tab'),
+            h5_file=h5_file,
+            dset_name='clean-bact/training1/extract/kmers/kmer_file2',
+            line_count=training_line_count)
+
+        read_tsv_write_h5_group(
+            tsv_fp=os.path.join(data_source_dir, 'clean-vir/training1/extract/kmers/kmer_file1.fasta.tab'),
+            h5_file=h5_file,
+            dset_name='clean-vir/training1/extract/kmers/kmer_file1.fasta.tab',
+            line_count=training_line_count)
+
+        read_tsv_write_h5_group(
+            tsv_fp=os.path.join(data_source_dir, 'clean-bact/training1/extract/kmers/kmer_file2.fasta.tab'),
+            h5_file=h5_file,
+            dset_name='clean-vir/training1/extract/kmers/kmer_file2.fasta.tab',
+            line_count=training_line_count)
+
+
+def read_tsv_write_h5_group(tsv_fp, h5_file, dset_name, line_count):
     t0 = time.time()
-    with h5py.File(h5_fp, 'w') as h5_file, open(tsv_fp, 'rt') as input_file:
+    with open(tsv_fp, 'rt') as input_file:
         input_header = input_file.readline().strip().split('\t')
         # do not store the first and last columns
         # store only kmer counts
@@ -37,6 +75,7 @@ def read_tsv_write_h5(tsv_fp, h5_fp, dset_name, line_count):
 
         print('wrote {} rows in {:5.2f}s'.format(dset.shape[0], time.time()-t0))
 
+
 t0 = time.time()
-read_tsv_write_h5(input_fp, h5_fp, dset_name, row_count)
-print('wrote {} rows to {} in {:5.2f}s'.format(row_count, h5_fp, time.time()-t0))
+write_all_training_and_testing_data()
+print('wrote all training and testing data in {:5.2f}s'.format(time.time()-t0))
