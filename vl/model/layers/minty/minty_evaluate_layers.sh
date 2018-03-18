2cd0fa5cfc750c44c6116ea73046f62034357a03
Joshua Lynch <jklynch@email.arizona.edu>
2018-03-18 15:14:44 -0400

    speed up iteration with fewer epochs

--- vl/model/layers/minty/minty_evaluate_layers.sh
+++ vl/model/layers/minty/minty_evaluate_layers.sh
@@ -7,6 +7,6 @@ VIRAL_LEARNING_DIR=~/host/project/viral-learning/
 time python ${VIRAL_LEARNING_DIR}/vl/model/layers/evaluate_network_depth.py \
     -i ${VIRAL_LEARNING_DIR}/data/perm_training_testing.h5 \
     -o ${VIRAL_LEARNING_DIR}/vl/model/layers/minty/output_dropout.pdf \
-    --epoch-count 10 \
-    --process-count 2 \
+    --epoch-count 2 \
+    --process-count 1 \
     --verbosity 1
