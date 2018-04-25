4dd73ffc1f888dfdaf7d4cb1e3e2d2e0bae451c9
Joshua Lynch <jklynch@email.arizona.edu>
2018-04-24 21:26:43 -0400

    initial commit

---
+++ vl/h5/genome/ocelote_to_h5.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#PBS -N genome_to_h5
+#PBS -m bea
+#PBS -M jklynch@email.arizona.edu
+#PBS -W group_list=bhurwitz
+#PBS -q standard
+#PBS -l select=1:ncpus=28:mem=168gb
+#PBS -l walltime=08:00:00
+
+source activate ktf
+
+cd ~/project/viral-learning/vl/h5/genome
+python to_h5.py
