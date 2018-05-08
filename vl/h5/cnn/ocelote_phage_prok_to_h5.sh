f64476f4796dbe14a5aacf296c29bdbd400cd568
Joshua Lynch <jklynch@email.arizona.edu>
2018-05-08 09:57:25 -0400

    increase walltime to 24 hours build 1000bp and 5000bp files as well as 500bp file

--- vl/h5/cnn/ocelote_phage_prok_to_h5.sh
+++ vl/h5/cnn/ocelote_phage_prok_to_h5.sh
@@ -6,17 +6,30 @@
 #PBS -W group_list=bhurwitz
 #PBS -q standard
 #PBS -l select=1:ncpus=28:mem=168gb
-#PBS -l walltime=08:00:00
+#PBS -l walltime=24:00:00
 
 source activate ktf
 
-mkdir -p data/cnn
-
 cd ~/project/viral-learning
+
 python vl/h5/cnn/write_cnn_h5_file.py \
     --phage-fp /rsgrps/bhurwitz/alise/my_data/Riveal_exp/Models/RefSeq_based_models/Prok_Phages_models/size_500pb/training_set/Phage_trainingset/phage500_training_set.fa \
     --prok-fp /rsgrps/bhurwitz/alise/my_data/Riveal_exp/Models/RefSeq_based_models/Prok_Phages_models/size_500pb/training_set/Proc_trainingset/proc500_training_set.fasta \
     --output-h5-fp /extra/jklynch/phage_prok_500.h5 \
     --image-width 100 \
-    --image-limit 100000
+    --image-limit 100000 &
+
+python vl/h5/cnn/write_cnn_h5_file.py \
+    --phage-fp /rsgrps/bhurwitz/alise/my_data/Riveal_exp/Models/RefSeq_based_models/Prok_Phages_models/size_1000pb/training_set/Phage_trainingset/phage1000_training_set.fa \
+    --prok-fp /rsgrps/bhurwitz/alise/my_data/Riveal_exp/Models/RefSeq_based_models/Prok_Phages_models/size_1000pb/training_set/Proc_trainingset/proc1000_training_set.fasta \
+    --output-h5-fp /extra/jklynch/phage_prok_1000.h5 \
+    --image-width 200 \
+    --image-limit 100000 &
+
+python vl/h5/cnn/write_cnn_h5_file.py \
+    --phage-fp /rsgrps/bhurwitz/alise/my_data/Riveal_exp/Models/RefSeq_based_models/Prok_Phages_models/size_5000pb/training_set/Phage_trainingset/phage5000_training_set.fa \
+    --prok-fp /rsgrps/bhurwitz/alise/my_data/Riveal_exp/Models/RefSeq_based_models/Prok_Phages_models/size_5000pb/training_set/Proc_trainingset/proc5000_training_set.fasta \
+    --output-h5-fp /extra/jklynch/phage_prok_5000.h5 \
+    --image-width 1000 \
+    --image-limit 100000 &
 
