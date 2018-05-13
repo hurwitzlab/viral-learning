49b6d4aa2138fe422637cf981e2228cf915d50bf
Joshua Lynch <jklynch@email.arizona.edu>
2018-05-13 10:44:49 -0500

    ask for 100 hours walltime

--- vl/h5/cnn/ocelote_phage_prok_1000_to_h5.sh
+++ vl/h5/cnn/ocelote_phage_prok_1000_to_h5.sh
@@ -6,7 +6,7 @@
 #PBS -W group_list=bhurwitz
 #PBS -q standard
 #PBS -l select=1:ncpus=28:mem=168gb
-#PBS -l walltime=08:00:00
+#PBS -l walltime=100:00:00
 
 source activate ktf
 
@@ -17,4 +17,4 @@ python vl/h5/cnn/write_cnn_h5_file.py \
     --prok-fp /rsgrps/bhurwitz/alise/my_data/Riveal_exp/Models/RefSeq_based_models/Prok_Phages_models/size_1000pb/training_set/Proc_trainingset/proc1000_training_set.fa \
     --output-h5-fp /extra/jklynch/phage_prok_1000.h5 \
     --image-width 500 \
-    --image-limit 200
+    --image-limit 100000
--- vl/h5/cnn/ocelote_phage_prok_5000_to_h5.sh
+++ vl/h5/cnn/ocelote_phage_prok_5000_to_h5.sh
@@ -6,7 +6,7 @@
 #PBS -W group_list=bhurwitz
 #PBS -q standard
 #PBS -l select=1:ncpus=28:mem=168gb
-#PBS -l walltime=08:00:00
+#PBS -l walltime=100:00:00
 
 source activate ktf
 
@@ -17,5 +17,5 @@ python vl/h5/cnn/write_cnn_h5_file.py \
     --prok-fp /rsgrps/bhurwitz/alise/my_data/Riveal_exp/Models/RefSeq_based_models/Prok_Phages_models/size_5000pb/training_set/Proc_trainingset/proc5000_training_set.fa \
     --output-h5-fp /extra/jklynch/phage_prok_5000.h5 \
     --image-width 2500 \
-    --image-limit 200
+    --image-limit 100000
 
