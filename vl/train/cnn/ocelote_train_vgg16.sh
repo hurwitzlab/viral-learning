c70d1470afac1758f02afeb6df2950acfb867e5c
Joshua Lynch <jklynch@email.arizona.edu>
2018-05-21 18:10:12 -0400

    reduce training and dev samples while testing

--- vl/train/cnn/ocelote_train_vgg16.sh
+++ vl/train/cnn/ocelote_train_vgg16.sh
@@ -16,7 +16,7 @@ cd ~/project/viral-learning/vl/train/cnn
 
 python train_vgg.py \
     --input-fp /extra/jklynch/phage_prok_500.h5 \
-    --train phage500_training_set.fa:0:80000,proc500_training_set.fasta:0:80000 \
-    --dev phage500_training_set.fa:80000:90000,proc500_training_set.fasta:80000:90000 \
+    --train phage500_training_set.fa:0:10000,proc500_training_set.fasta:0:10000 \
+    --dev phage500_training_set.fa:80000:81000,proc500_training_set.fasta:80000:81000 \
     --test phage500_training_set.fa:90000:,proc500_training_set.fasta:90000: \
     --epochs 1
