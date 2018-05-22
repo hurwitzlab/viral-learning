002710c829732fde3b84b8f1fab6bd0dc350d71f
Joshua Lynch <jklynch@email.arizona.edu>
2018-05-21 21:39:35 -0400

    3 epochs

--- vl/train/cnn/ocelote_train_vgg16.sh
+++ vl/train/cnn/ocelote_train_vgg16.sh
@@ -19,4 +19,4 @@ python train_vgg.py \
     --train phage500_training_set.fa:0:10000,proc500_training_set.fasta:0:10000 \
     --dev phage500_training_set.fa:80000:81000,proc500_training_set.fasta:80000:81000 \
     --test phage500_training_set.fa:90000:,proc500_training_set.fasta:90000: \
-    --epochs 1
+    --epochs 3
